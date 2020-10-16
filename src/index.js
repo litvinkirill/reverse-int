eb5fc2e215ad1fd82d75e1f0e32dea4cc3dea923
Kirill Litvin <litvin.kirill.m@mail.ru>
2020-10-16 15:13:36 +0200

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let m = Math.abs(n); // возмем модуль числа (избавимся от "-"" если он будет)
+    let str = m.toString(); // превратим число в строку
+    let stra = str.split(''); // разделим строку на элементы
+    let strRev = stra.reverse(); // перевернем элементы строки
+    let stringRev = strRev.join(''); // склеим элементы строки
+    let numberReversed = parseInt(stringRev); // превратим строку в число
+    return numberReversed;
 }
